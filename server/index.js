02dbfdc21c5701b046765dcd4b52d4f3804d984d
chempox <jpy20@outlook.com>
2021-08-15 14:43:23 -0500

    Login, Register & Hashing working

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const mysql = require("mysql");
 const cors = require('cors');
 const { response } = require("express");
 const app = express();
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 //Cors & Express init
 app.use(cors());
@@ -23,32 +25,44 @@ app.post('/signup', (require,response) =>{
     const username = require.body.username;
     const email = require.body.email;
     const password = require.body.password;
-
-    db.query('INSERT INTO user (username, password, name, lastName, email) VALUES (?,?,?,?,?)',
-    [username,password,name,lastName,email],
-    (err,result) =>{
-        if(err){
-            console.log(err)
-        }else{
-            response.send("Values Inserted")
+    
+    bcrypt.hash(password,saltRounds, (err,hash) =>{
+        if (err){
+            console.log(err);
         }
-    }
-    );
+        db.query('INSERT INTO user (username, password, name, lastName, email) VALUES (?,?,?,?,?)',
+        [username,hash,name,lastName,email],
+        (err,result) =>{
+            if(err){
+                console.log(err)
+            }else{
+                response.send("Values Inserted")
+            }
+        }
+        );
+    })
+    
 });
 
 app.post('/login',(require,response) =>{
     const username = require.body.username;
     const password = require.body.password;
-    db.query('SELECT * FROM user WHERE username = ? AND password = ?',
-    [username,password],
+    db.query("SELECT * FROM user WHERE username = ?;",
+    username,
     (err, result) =>{
         if (err) {
             response.send({err: err});
         }
         if(result.length > 0) {
-            response.send(result);
+            bcrypt.compare(password, result[0].password, (error, res) => {
+                if(res){
+                    response.send(result)
+                }else{
+                    response.send({message: "Wrong password or username"});
+                }
+            })
         } else{
-            response.send({message: "Wrong password or username"});
+            response.send({message: "User doesn't exist"});
         }
     })
 })
